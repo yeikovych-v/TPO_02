cb01d5e17410e15dcea94287428c834d848c9ebe
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-16 14:35:39 +0100

    Removed deprecated files.

--- src/main/java/pl/edu/s28201/tpo_02/exception/FileFormatException.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.s28201.tpo_02.exception;
-
-public class FileFormatException extends RuntimeException{
-    public FileFormatException(String message) {
-        super(message);
-    }
-}
--- src/main/java/pl/edu/s28201/tpo_02/service/PrintingService.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.s28201.tpo_02.service;
-
-import pl.edu.s28201.tpo_02.model.Entry;
-
-public interface PrintingService {
-
-    String process(String word);
-}
